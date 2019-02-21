8aff16d78a94ee8426e711d3c2b480b6295d0269
Adrianbarros <41019238+Adrianbarros@users.noreply.github.com>
2019-02-20 23:03:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -135,6 +135,6 @@ def logout():
     resp.set_cookie('sillyauth',  expires=0)
     return resp
 
-if __name__=="__main__":
-	app.run(debug=True)
+if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, threaded=True)
